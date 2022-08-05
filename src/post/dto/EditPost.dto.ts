7a2a803267298ba567fa013792913bb18a51b29e
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-08-05 15:18:37 +1000

    edit edit post dto

--- src/post/dto/EditPost.dto.ts
+++ src/post/dto/EditPost.dto.ts
@@ -1,14 +1,47 @@
 import {
+  ApiProperty,
   OmitType,
   PartialType,
 } from '@nestjs/swagger';
+import { DraftStatus } from '@prisma/client';
+import {
+  IsDateString,
+  IsEnum,
+  IsObject,
+  IsOptional,
+  IsString,
+  Matches,
+} from 'class-validator';
 import { PostDto } from './Post.dto';
 
-export class EditPostDto extends PartialType(
-  OmitType(PostDto, [
-    'createdAt',
-    'id',
-    'updatedAt',
-    'userId',
-  ] as const),
-) {}
+export class EditPostDto {
+  @IsEnum(DraftStatus)
+  @ApiProperty({ enum: DraftStatus })
+  @IsOptional()
+  status?: DraftStatus;
+
+  @ApiProperty()
+  @Matches(/^[a-z0-9]+(?:[-/][a-z0-9]+)*$/)
+  @IsOptional()
+  slug?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title?: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  summary?: string;
+
+  @IsObject()
+  @ApiProperty()
+  @IsOptional()
+  content?: object;
+
+  @IsDateString()
+  @ApiProperty()
+  @IsOptional()
+  publishedAt?: Date;
+}
